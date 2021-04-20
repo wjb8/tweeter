079e60ec7fe1624571e27159340d5dd947dcae7a
James Brown <brownj95@gmail.com>
2021-04-20 19:40:02 +0000

    make char counter red if chars exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,15 @@ $('#tweet-text').on('input', function() {
   let chars = this.value.length;
   let remainingChars = 140 - chars;
   let counter = $(this).parent().children('.ctrl').children('.counter');
-  console.log(counter[0].innerText = remainingChars);
+  counter[0].innerText = remainingChars;
+
+  if (remainingChars <= 0) {
+    $(counter[0]).css('color', 'red')
+  }
+
+  if (remainingChars > 0) {
+    $(counter[0]).css('color', 'rgb(107, 107, 107)')
+  }
 
 })
 
